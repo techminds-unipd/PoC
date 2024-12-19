2bf87f86aac60cffe2ac25924c0f4378391708ab
Tommaso Lazzarin <36604344+tlazzarin@users.noreply.github.com>
2024-12-19 18:04:24 +0100

    Tentativo con openai

--- backend/poc-backend/src/workflows/agent/agent.service.ts
+++ backend/poc-backend/src/workflows/agent/agent.service.ts
@@ -14,6 +14,7 @@ import {
   GmailSendMessage,
 } from '@langchain/community/tools/gmail';
 import { initializeAgentExecutorWithOptions } from 'langchain/agents';
+import { OpenAI } from '@langchain/openai';
 
 @Injectable()
 export class AgentService {
@@ -23,8 +24,7 @@ export class AgentService {
     const gmailParams: GmailBaseToolParams = {
       credentials: {
         clientEmail: 'techminds.unipd@gmail.com',
-        privateKey: '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',
-        //keyfile: './token.json',
+        privateKey: '',
       },
       scopes: ['https://www.googleapis.com/auth/gmail.readonly'],
     };
@@ -37,7 +37,7 @@ export class AgentService {
       new GmailSendMessage(gmailParams),
     ];
     const model = new ChatGroq({
-      model: 'llama-3.1-70b-versatile',
+      model: 'llama3-groq-70b-8192-tool-use-preview',
       temperature: 0,
     });
 
@@ -46,9 +46,45 @@ export class AgentService {
       verbose: true,
     });
 
-    const input = "Riesci a leggere le mail presenti nell'inbox usando le credenziali fornite?";
+    const input =
+      "Riesci a leggere le mail presenti nell'inbox usando le credenziali fornite?";
     const createResult = await gmailAgent.invoke({ input: input });
     console.log('Create result', createResult);
+    return { status: 'executed' };
+  }
+
+  async execute_openai(workflow: WorkflowDocument): Promise<Record<string, any>> {
+    const model = new OpenAI({
+      temperature: 0,
+      apiKey: this.configService.get<string>('OPENAI_API_KEY'),
+    });
+
+    const gmailParams: GmailBaseToolParams = {
+      credentials: {
+        clientEmail: 'techminds.unipd@gmail.com',
+        privateKey: '',
+        keyfile: './token.json',
+      },
+      scopes: ['https://www.googleapis.com/auth/gmail.readonly'],
+    };
+
+    const tools: StructuredTool[] = [
+      new GmailGetMessage(gmailParams),
+      new GmailCreateDraft(gmailParams),
+      new GmailGetThread(gmailParams),
+      new GmailSearch(gmailParams),
+      new GmailSendMessage(gmailParams),
+    ];
+
+    const gmailAgent = await initializeAgentExecutorWithOptions(tools, model, {
+      agentType: 'structured-chat-zero-shot-react-description',
+      verbose: true,
+    });
+    const input =
+      "Riesci a leggere le mail presenti nell'inbox usando le credenziali fornite?";
+    const createResult = await gmailAgent.invoke({ input: input });
+    console.log('Create result', createResult);
+
     return { status: 'executed' };
   }
 }
