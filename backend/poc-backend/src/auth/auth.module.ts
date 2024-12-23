a71cd47533b627503b2b1479227022ee7cbaa8c6
Giuseppe Tutino <giuseppetutino1@gmail.com>
2024-12-23 18:51:03 +0100

    Aggiunto schema per i dati utente #8

--- backend/poc-backend/src/auth/auth.module.ts
+++ backend/poc-backend/src/auth/auth.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { GoogleStrategy } from './strategy/google.strategy';
+import { User, UserSchema } from 'src/schemas/user.schema';
 
 @Module({
   imports: [
@@ -10,6 +12,9 @@ import { GoogleStrategy } from './strategy/google.strategy';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+    ]),
   ],
   controllers: [AuthController],
   providers: [ AuthService,GoogleStrategy,
